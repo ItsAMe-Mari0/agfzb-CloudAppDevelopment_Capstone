12a0e1b403acbf38ed0c2331b6df2450aa3a5a7b
Mari0 <bradstoor.spam@gmail.com>
2023-11-15 09:23:55 -0500

    Updating for step 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,8 +11,10 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
+    path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
 
