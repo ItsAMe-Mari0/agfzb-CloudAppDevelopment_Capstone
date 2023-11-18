33f056f25a3f0bd021cba25046e903dd2f509aa6
ItsAMe-Mari0 <146299168+ItsAMe-Mari0@users.noreply.github.com>
2023-11-17 19:30:49 -0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
 from . import views
 
 app_name = 'djangoapp'
@@ -11,21 +12,30 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
-    path(route='about/', view=views.about, name='about'),
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
-    path(route='contact/', view=views.contact, name='contact'),
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
+    
     # path for login
-
+    path(route='login', view=views.login_request, name='login'),
+    path('login/', views.login_request, name='login'),
+    
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path(route='index', view=views.get_dealerships, name='index'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    
     # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path(route='', view=views.get_dealerships, name='index'),
+    path('dealer/<int:dealer_id>/add_review/', views.add_review, name='add_review'),
+    
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
